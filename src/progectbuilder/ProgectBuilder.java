5998f76609df95f46d0491009706702415917f5c
Simone <picco92.simone@gmail.com>
2015-02-17 21:24:27 +0100

    Definizione linguaggio v1

--- src/progectbuilder/ProgectBuilder.java
+++ src/progectbuilder/ProgectBuilder.java
@@ -15,5 +15,28 @@ public class ProgectBuilder {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        /*
+        Language
+        
+        NomeClasse .ClasseEstesa -ClasseImplementata1 ClasseImplementata2 [ecc]
+        .[variabili: una per riga]
+        [metodi: uno per riga]
+        
+        variabile
+        [visibilità]Nome
+        
+        metodi
+        [visibilità]NomeMetodo [return]
+        
+        visibilità
+        default protected
+        + public
+        - private
+        
+        return
+        un qualsiasi tipo (anche non standard: nessun controllo)
+        
+        */
     }
 }
